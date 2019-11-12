81afbee085c331a144a17b0998d6b45d27cd070d
Danielle Matton <danimatton@gmail.com>
2019-11-12 18:32:18 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,7 +31,8 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   user_response = gets.strip
-  if (1..9).to_a.include?(user_respoonse.to_i)
+
+  songs.each_with_index.to_i
     puts "Playing #{songs[user_response.to_i - 1]}"
   elsif songs.include?(user_response)
     puts "Playing #{user_response}"
