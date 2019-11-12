5568b12ec6fe8649240758b1f312a3c8e3c5acc5
Danielle Matton <danimatton@gmail.com>
2019-11-12 18:24:42 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -34,7 +34,7 @@ def play(songs)
   if (1..9).to_a.include?(user_respoonse.to_i)
     puts "Playing #{songs[user_response.to_i-1]}"
   elsif songs.include?(user_response)
-    puts "Playing #{song[index]}"
+    puts "Playing #{user_response}"
   else
     puts "Invalid input, please try again."
   end
